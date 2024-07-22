223e3e99ad2d466a8ec86192fc5cabed9e644a53
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-22 22:27:40 +0530

    improve app size

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={cn(manrope.className, "antialiased max-w-xl mx-14 mt-8 lg:mx-auto")}>
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-            
+      <body className={cn(manrope.className, "antialiased max-w-xl md:mx-14 mx-10 mt-8 lg:mx-auto")}>
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">  
             {children}
             <Footer />
           </main>
