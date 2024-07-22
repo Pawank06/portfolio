0d8f5e01b81b585aac6d84187b84dea0ce4d3b70
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-22 22:33:25 +0530

    add link

--- components/footer.tsx
+++ components/footer.tsx
@@ -51,7 +51,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/Pawank06"
+            href="https://github.com/Pawank06/portfolio"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">source</p>
