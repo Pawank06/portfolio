6e8369283370b5baee166983a9fb0afcbd6e553b
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-22 21:16:37 +0530

    work on footer

---
+++ components/footer.tsx
@@ -0,0 +1,64 @@
+
+
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default function Footer() {
+  return (
+    <footer className="mb-16 md:mb-0">
+      <ul className="font-sm mt-8 flex items-center text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <div className="flex gap-6">
+
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://x.com/near0lx"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">x</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/Pawank06"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">github</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/Pawank06"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">source</p>
+          </a>
+        </li>
+        </div>
+      </ul>
+    </footer>
+  )
+}
