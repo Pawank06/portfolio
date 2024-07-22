c92f0dfd48801c234d67baf81b050ac37da5e2ed
PawanK06 <pawan06kumar2003@gmail.com>
2024-07-22 22:32:58 +0530

    remove italic

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,12 @@ export default function Page() {
       </h1>
       
       <p className="mb-8 opacity-60 font-semibold">
-        co-founder <Link className='italic underline' href="">@.viralxpost</Link> + <Link className='italic underline' href="">@.fpg</Link></p>
+        co-founder <Link className='underline' href="">viralxpost</Link> + <Link className='underline' href="">fpg</Link></p>
       <p className="mb-4">
         I am a 21 year old developer based in india.
       </p>
       <p className="mb-4">
-      I&apos;m also a <span className='font-semibold italic'>CS</span> undergrad passionate about building <span className='font-semibold italic'>SaaS</span> applications, with a strong interest in <span className='font-semibold italic'>web3</span> and <span className='font-semibold italic'>AI</span> technologies.
+      I&apos;m also a <span className='font-semibold'>CS</span> undergrad passionate about building <span className='font-semibold'>SaaS</span> applications, with a strong interest in <span className='font-semibold'>web3</span> and <span className='font-semibold'>AI</span> technologies.
       </p>
       <h1 className="mb-8 mt-16 md:text-5xl text-3xl font-bold  tracking-tighter">
       building a saas?
@@ -28,12 +28,12 @@ export default function Page() {
       The fastest way to reach me is by messaging me on X at <Link href="" className='font-semibold italic underline'>@near0lx</Link>
       </p>
       <Separator className='my-16'/>
-      <div className='px-4 dark:text-zinc-700'>
+      <ul className=' dark:text-zinc-700'>
       <li className='pb-4'>twitter <Link className='underline opacity-70' href="https://x.com/near0lx">@near0lx</Link></li>
       <li className='pb-4'>github <Link className='underline opacity-70' href="https://github.com/Pawank06">@pawank06</Link></li>
-      <li className='pb-4'>Instagram <Link className='underline opacity-70' href="https://www.instagram.com/aka.pawan/">aka.pawan</Link></li>
-      <li className='pb-4'>email <Link className='underline opacity-70' href="https://heypawanb@gmail.com">@heypawanb@gmail.com</Link></li>
-      </div>
+      <li className='pb-4'>Instagram <Link className='underline opacity-70' href="https://www.instagram.com/aka.pawan/">@aka.pawan</Link></li>
+      <li className='pb-4 md:block hidden'>email <Link className='underline opacity-70' href="https://heypawanb@gmail.com">@heypawanb@gmail.com</Link></li>
+      </ul>
     </section>
   )
 }
